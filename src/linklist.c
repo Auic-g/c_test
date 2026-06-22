43ffd4492092c7e4476447f3f6fce7aeef23b49e
Auic-g <3233709963@qq.com>
2026-06-22 21:26:27 +0800

    feat(linklist): 添加多个链表操作函数，包括删除、逆置、拆分和查找公共元素

--- src/linklist.c
+++ src/linklist.c
@@ -119,3 +119,201 @@ bool ListDelete(LinkList *L, int i, LinkElemType *e){
     return true;
 }
 
+
+
+
+
+
+
+
+//Problem
+//1.删除所有值为x的节点（带头结点单链表）
+bool Del_x(LinkList *L, LinkElemType x){
+    LNode *p = *L, *q;
+    while(p->next != NULL){
+        if(p->next->data == x){
+            q = p->next;
+            p->next = q->next;
+            free(q);
+        }
+    }
+    return true;
+}
+
+//2.删除最小值节点(带头结点单链表)
+bool Del_Min(LinkList *L){
+    LNode *p = *L, *q = p->next,  *min = p->next;
+    while(p->next != NULL){
+        if(p->next->data < min->data){
+            min = p->next;
+            q = p;
+        }
+        p = p->next;
+    }
+    q->next = min->next;
+    free(min);
+    return true;
+    
+}
+
+//3.将带头结点的单链表逆置，空间复杂度O(1)
+LinkList Reverse(LinkList *L){
+    LNode *p = *L, *q = p->next, *r;
+    p->next = NULL;
+    while(q != NULL){
+        r = q->next;
+        q->next = p;
+        p = q;
+        q = r;
+    }
+    *L = p;
+    return *L;
+}
+
+//4.删除处于无序的带头结点单链表中的给定的两个值之间的元素
+bool Del_x_y(LinkList *L, LinkElemType x, LinkElemType y){
+    if(x >= y || (*L)->next == NULL) return false;
+    LNode *p = *L, *q = p->next, *r;
+    while(q != NULL){
+        if(q->data >= x && q->data <= y){
+            r = q;
+            q = q->next;
+            p->next = q;
+            free(r);
+        }
+        else{
+            p = q;
+            q = q->next;
+        }
+    }
+    return true;
+}
+
+//5.分析找出两个单链表公共节点的思想
+
+//6.将一个单链表拆为两个，空间复杂度O(1)
+LinkList Split(LinkList *L){
+    LinkList A, B;
+    InitList(&A);
+    InitList(&B);
+    LNode *p = *L, *a = A, *b = B, *q = p->next, *r;
+    while(q != NULL){
+        q = p->next;
+        r = q->next;
+        p->next = q->next;
+        a->next = q;
+        p->next = r->next;
+        b->next = r;
+        a = q;
+        b = r;
+    }
+    return A;
+    return B;
+}
+
+//7.删除递增有序单链表中的重复元素
+LinkList Del_Repeted(LinkList *L){
+    LNode *p = *L, *q = p->next, *r;
+    while(q != NULL){
+        if(q->data == p->data){
+            r = q;
+            q = q->next;
+            p->next = q;
+            free(r);
+        }
+        else{
+            p = q;
+            q = q->next;
+        }
+    }
+    return *L;
+}
+
+//8.将两个递增有序的单链表中的公共元素产生一个单链表，不破坏原来两个单链表的元素
+LinkList Find_Common_Elem(LinkList *A, LinkList *B){
+    LinkList C;
+    InitList(&C);
+    LNode *p = *A, *q = *B, *r = C, *s;
+    while(p != NULL && q != NULL){
+        if(p->data == q->data){
+            s = (LNode*)malloc(sizeof(LNode));
+            s->data = p->data;
+            r->next = s;
+            r = s;
+            p = p->next;
+            q = q->next;
+        }
+        else if(p->data < q->data){
+            p = p->next;
+        }
+        else{
+            q = q->next;
+        }
+        r->next = NULL;
+    }
+}
+
+//9.将两个递增有序的单链表的交集放在第一个单链表中
+LinkList Find_Intersection(LinkList *A, LinkList *B){
+    LNode *p = *A, *q = *B, *r;
+    while(p != NULL && q != NULL){
+        if(p->data == q->data){
+            p = p->next;
+            q = q->next;
+        }
+        else if(p->data < q->data){
+            r = p;
+            p = p->next;
+            free(r);
+        }
+        else{
+            q = q->next;
+            if(q == NULL){
+                while(p != NULL){
+                    r = p;
+                    p = p->next;
+                    free(r);
+                }
+            }
+        }
+    }
+    return *A;
+}
+
+//10.判断一个单链表的整数序列是否是另一个的连续子序列
+bool Is_SubList(LinkList *A, LinkList *B){
+    LNode *p = *A, *q = *B;
+    while(p != NULL && q != NULL){
+        if(p->data == q->data){
+            p = p->next;
+            q = q->next;
+        }
+        else if(p->data < q->data){
+            p = p->next;
+        }
+        else{
+            return false;
+        }
+    }
+    return true;
+}
+
+//11.判断带头结点的循环双链表是否对称
+bool Is_Symmetry(DLinkList *L){
+    DNode *p = (*L)->next, *q = (*L)->prior;
+    while(p != q && p->next != q){
+        if(p->data != q->data){
+            return false;
+        }
+        p = p->next;
+        q = q->prior;
+    }
+    return true;
+}
+
+//12.编写一个函数将一个循环单链表的头指针h1链接到另一个循环单链表的头指针h2后，要求链接后的链表仍保持循环单链表形式
+void Link_List(LinkList *h1, LinkList *h2){ 
+    LNode *p = *h1, *q = *h2;
+    
+}
+
